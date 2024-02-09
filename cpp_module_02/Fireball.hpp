f138fd47e6406607ce522a7d0ff6358cad868ecd
Edgar alexandre Freire marques <efreire-@c2r1s7.42lisboa.com>
2024-02-09 18:32:06 +0000

    02 #2

---
+++ cpp_module_02/Fireball.hpp
@@ -0,0 +1,20 @@
+#ifndef FIREBALL_HPP
+# define FIREBALL_HPP
+
+# include <iostream>
+# include <string>
+
+# include "ASpell.hpp"
+
+class ASpell;
+
+class Fireball: public ASpell
+{
+	public:
+		Fireball(void);
+		~Fireball(void);
+
+		ASpell	*clone(void) const;
+};
+
+#endif
